006466b5c6356deddaed13c246067905d2c08e9d
fedoco <fedoco@users.noreply.github.com>
2018-01-04 12:13:55 +0100

    Rename parser variables to make for clearer diffs when command line options are modified

--- reporter.py
+++ reporter.py
@@ -194,14 +194,14 @@ def output_result(result, unzip = True):
 def parse_arguments():
     """Build and parse the command line arguments"""
 
-    parser = argparse.ArgumentParser(description="Reporting tool for querying Sales- and Financial Reports from iTunes Connect", epilog="For a detailed description of report types, see http://help.apple.com/itc/appssalesandtrends/#/itc37a18bcbf")
+    parser_main = argparse.ArgumentParser(description="Reporting tool for querying Sales- and Financial Reports from iTunes Connect", epilog="For a detailed description of report types, see http://help.apple.com/itc/appssalesandtrends/#/itc37a18bcbf")
 
     # (most of the time) optional arguments
-    parser.add_argument('-a', '--account', type=int, help="account number (needed if your Apple ID has access to multiple accounts; for a list of your account numbers, use the 'getAccounts' command)")
-    parser.add_argument('-m', '--mode', choices=['Normal', 'Robot.XML'], default='Normal', help="output format: plain text or XML (defaults to '%(default)s')")
+    parser_main.add_argument('-a', '--account', type=int, help="account number (needed if your Apple ID has access to multiple accounts; for a list of your account numbers, use the 'getAccounts' command)")
+    parser_main.add_argument('-m', '--mode', choices=['Normal', 'Robot.XML'], default='Normal', help="output format: plain text or XML (defaults to '%(default)s')")
 
     # always required arguments
-    required_args = parser.add_argument_group("required arguments")
+    required_args = parser_main.add_argument_group("required arguments")
     required_args.add_argument('-u', '--userid', required=True, help="Apple ID for use with iTunes Connect")
 
     # template for commands that require authentication with password
@@ -221,83 +221,83 @@ def parse_arguments():
     mutex_group.add_argument('-T', '--access-token', help='iTunes Connect access token (can be obtained with the generateToken command or via iTunes Connect -> Sales & Trends -> Reports -> About Reports)')
 
     # commands
-    subparsers = parser.add_subparsers(dest='command', title='commands', description="Specify the task you want to be carried out (use -h after a command's name to get additional help for that command)")
+    subparsers = parser_main.add_subparsers(dest='command', title='commands', description="Specify the task you want to be carried out (use -h after a command's name to get additional help for that command)")
 
-    parser_01 = subparsers.add_parser('getStatus', help="check if iTunes Connect is available for queries", parents=[parser_auth_token])
-    parser_01.add_argument('service', choices=['Sales', 'Finance'], help="service endpoint to query")
-    parser_01.set_defaults(func=itc_get_status)
+    parser_cmd = subparsers.add_parser('getStatus', help="check if iTunes Connect is available for queries", parents=[parser_auth_token])
+    parser_cmd.add_argument('service', choices=['Sales', 'Finance'], help="service endpoint to query")
+    parser_cmd.set_defaults(func=itc_get_status)
 
-    parser_02 = subparsers.add_parser('getAccounts', help="fetch a list of accounts accessible to the Apple ID given in -u", parents=[parser_auth_token])
-    parser_02.add_argument('service', choices=['Sales', 'Finance'], help="service endpoint to query")
-    parser_02.set_defaults(func=itc_get_accounts)
+    parser_cmd = subparsers.add_parser('getAccounts', help="fetch a list of accounts accessible to the Apple ID given in -u", parents=[parser_auth_token])
+    parser_cmd.add_argument('service', choices=['Sales', 'Finance'], help="service endpoint to query")
+    parser_cmd.set_defaults(func=itc_get_accounts)
 
-    parser_03 = subparsers.add_parser('getVendors', help="fetch a list of vendors accessible to the Apple ID given in -u", parents=[parser_auth_token])
-    parser_03.set_defaults(func=itc_get_vendors)
+    parser_cmd = subparsers.add_parser('getVendors', help="fetch a list of vendors accessible to the Apple ID given in -u", parents=[parser_auth_token])
+    parser_cmd.set_defaults(func=itc_get_vendors)
 
-    parser_04 = subparsers.add_parser('getVendorsAndRegions', help="fetch a list of financial reports you can download by vendor number and region", parents=[parser_auth_token])
-    parser_04.set_defaults(func=itc_get_vendor_and_regions)
+    parser_cmd = subparsers.add_parser('getVendorsAndRegions', help="fetch a list of financial reports you can download by vendor number and region", parents=[parser_auth_token])
+    parser_cmd.set_defaults(func=itc_get_vendor_and_regions)
 
-    parser_05 = subparsers.add_parser('getFinancialReport', help="download a financial report file for a specific region and fiscal period", parents=[parser_auth_token])
-    parser_05.add_argument('vendor', type=int, help="vendor number of the report to download (for a list of your vendor numbers, use the 'getVendors' command)")
-    parser_05.add_argument('regioncode', help="two-character code of country of the report to download (for a list of country codes by vendor number, use the 'getVendorsAndRegions' command)")
-    parser_05.add_argument('fiscalyear', help="four-digit year of the report to download (year is specific to Apple’s fiscal calendar)")
-    parser_05.add_argument('fiscalperiod', help="period in fiscal year for the report to download (1-12; period is specific to Apple’s fiscal calendar)")
-    parser_05.set_defaults(func=itc_get_financial_report)
+    parser_cmd = subparsers.add_parser('getFinancialReport', help="download a financial report file for a specific region and fiscal period", parents=[parser_auth_token])
+    parser_cmd.add_argument('vendor', type=int, help="vendor number of the report to download (for a list of your vendor numbers, use the 'getVendors' command)")
+    parser_cmd.add_argument('regioncode', help="two-character code of country of the report to download (for a list of country codes by vendor number, use the 'getVendorsAndRegions' command)")
+    parser_cmd.add_argument('fiscalyear', help="four-digit year of the report to download (year is specific to Apple’s fiscal calendar)")
+    parser_cmd.add_argument('fiscalperiod', help="period in fiscal year for the report to download (1-12; period is specific to Apple’s fiscal calendar)")
+    parser_cmd.set_defaults(func=itc_get_financial_report)
 
-    parser_06 = subparsers.add_parser('getSalesReport', help="download a summary sales report file for a specific date range", parents=[parser_auth_token])
-    parser_06.add_argument('vendor', type=int, help="vendor number of the report to download (for a list of your vendor numbers, use the 'getVendors' command)")
-    parser_06.add_argument('datetype', choices=['Daily', 'Weekly', 'Monthly', 'Yearly'], help="length of time covered by the report")
-    parser_06.add_argument('date', help="specific time covered by the report (weekly reports use YYYYMMDD, where the day used is the Sunday that week ends; monthly reports use YYYYMM; yearly reports use YYYY)")
-    parser_06.set_defaults(func=itc_get_sales_report)
+    parser_cmd = subparsers.add_parser('getSalesReport', help="download a summary sales report file for a specific date range", parents=[parser_auth_token])
+    parser_cmd.add_argument('vendor', type=int, help="vendor number of the report to download (for a list of your vendor numbers, use the 'getVendors' command)")
+    parser_cmd.add_argument('datetype', choices=['Daily', 'Weekly', 'Monthly', 'Yearly'], help="length of time covered by the report")
+    parser_cmd.add_argument('date', help="specific time covered by the report (weekly reports use YYYYMMDD, where the day used is the Sunday that week ends; monthly reports use YYYYMM; yearly reports use YYYY)")
+    parser_cmd.set_defaults(func=itc_get_sales_report)
 
-    parser_07 = subparsers.add_parser('getSubscriptionReport', help="download a subscription report file for a specific day", parents=[parser_auth_token])
-    parser_07.add_argument('vendor', type=int, help="vendor number of the report to download (for a list of your vendor numbers, use the 'getVendors' command)")
-    parser_07.add_argument('date', help="specific day covered by the report (use YYYYMMDD format)")
-    parser_07.set_defaults(func=itc_get_subscription_report)
+    parser_cmd = subparsers.add_parser('getSubscriptionReport', help="download a subscription report file for a specific day", parents=[parser_auth_token])
+    parser_cmd.add_argument('vendor', type=int, help="vendor number of the report to download (for a list of your vendor numbers, use the 'getVendors' command)")
+    parser_cmd.add_argument('date', help="specific day covered by the report (use YYYYMMDD format)")
+    parser_cmd.set_defaults(func=itc_get_subscription_report)
 
-    parser_08 = subparsers.add_parser('getSubscriptionEventReport', help="download an aggregated subscriber activity report file for a specific day", parents=[parser_auth_token])
-    parser_08.add_argument('vendor', type=int, help="vendor number of the report to download (for a list of your vendor numbers, use the 'getVendors' command)")
-    parser_08.add_argument('date', help="specific day covered by the report (use YYYYMMDD format)")
-    parser_08.set_defaults(func=itc_get_subscription_event_report)
+    parser_cmd = subparsers.add_parser('getSubscriptionEventReport', help="download an aggregated subscriber activity report file for a specific day", parents=[parser_auth_token])
+    parser_cmd.add_argument('vendor', type=int, help="vendor number of the report to download (for a list of your vendor numbers, use the 'getVendors' command)")
+    parser_cmd.add_argument('date', help="specific day covered by the report (use YYYYMMDD format)")
+    parser_cmd.set_defaults(func=itc_get_subscription_event_report)
 
-    parser_09 = subparsers.add_parser('getSubscriberReport', help="download a transaction-level subscriber activity report file for a specific day", parents=[parser_auth_token])
-    parser_09.add_argument('vendor', type=int, help="vendor number of the report to download (for a list of your vendor numbers, use the 'getVendors' command)")
-    parser_09.add_argument('date', help="specific day covered by the report (use YYYYMMDD format)")
-    parser_09.set_defaults(func=itc_get_subscriber_report)
+    parser_cmd = subparsers.add_parser('getSubscriberReport', help="download a transaction-level subscriber activity report file for a specific day", parents=[parser_auth_token])
+    parser_cmd.add_argument('vendor', type=int, help="vendor number of the report to download (for a list of your vendor numbers, use the 'getVendors' command)")
+    parser_cmd.add_argument('date', help="specific day covered by the report (use YYYYMMDD format)")
+    parser_cmd.set_defaults(func=itc_get_subscriber_report)
 
-    parser_10 = subparsers.add_parser('getNewsstandReport', help="download a magazines & newspapers report file for a specific date range", parents=[parser_auth_token])
-    parser_10.add_argument('vendor', type=int, help="vendor number of the report to download (for a list of your vendor numbers, use the 'getVendors' command)")
-    parser_10.add_argument('datetype', choices=['Daily', 'Weekly'], help="length of time covered by the report")
-    parser_10.add_argument('date', help="specific time covered by the report (weekly reports, like daily reports, use YYYYMMDD, where the day used is the Sunday that week ends")
-    parser_10.set_defaults(func=itc_get_newsstand_report)
+    parser_cmd = subparsers.add_parser('getNewsstandReport', help="download a magazines & newspapers report file for a specific date range", parents=[parser_auth_token])
+    parser_cmd.add_argument('vendor', type=int, help="vendor number of the report to download (for a list of your vendor numbers, use the 'getVendors' command)")
+    parser_cmd.add_argument('datetype', choices=['Daily', 'Weekly'], help="length of time covered by the report")
+    parser_cmd.add_argument('date', help="specific time covered by the report (weekly reports, like daily reports, use YYYYMMDD, where the day used is the Sunday that week ends")
+    parser_cmd.set_defaults(func=itc_get_newsstand_report)
 
-    parser_11 = subparsers.add_parser('getOptInReport', help="download contact information for customers who opt in to share their contact information with you", parents=[parser_auth_token])
-    parser_11.add_argument('vendor', type=int, help="vendor number of the report to download (for a list of your vendor numbers, use the 'getVendors' command)")
-    parser_11.add_argument('date', help="specific day covered by the report (use YYYYMMDD format)")
-    parser_11.set_defaults(func=itc_get_opt_in_report)
+    parser_cmd = subparsers.add_parser('getOptInReport', help="download contact information for customers who opt in to share their contact information with you", parents=[parser_auth_token])
+    parser_cmd.add_argument('vendor', type=int, help="vendor number of the report to download (for a list of your vendor numbers, use the 'getVendors' command)")
+    parser_cmd.add_argument('date', help="specific day covered by the report (use YYYYMMDD format)")
+    parser_cmd.set_defaults(func=itc_get_opt_in_report)
 
-    parser_12 = subparsers.add_parser('getPreOrderReport', help="download a summary report file of pre-ordered items for a specific date range", parents=[parser_auth_token])
-    parser_12.add_argument('vendor', type=int, help="vendor number of the report to download (for a list of your vendor numbers, use the 'getVendors' command)")
-    parser_12.add_argument('datetype', choices=['Daily', 'Weekly', 'Monthly', 'Yearly'], help="length of time covered by the report")
-    parser_12.add_argument('date', help="specific time covered by the report (weekly reports use YYYYMMDD, where the day used is the Sunday that week ends; monthly reports use YYYYMM; yearly reports use YYYY)")
-    parser_12.set_defaults(func=itc_get_pre_order_report)
+    parser_cmd = subparsers.add_parser('getPreOrderReport', help="download a summary report file of pre-ordered items for a specific date range", parents=[parser_auth_token])
+    parser_cmd.add_argument('vendor', type=int, help="vendor number of the report to download (for a list of your vendor numbers, use the 'getVendors' command)")
+    parser_cmd.add_argument('datetype', choices=['Daily', 'Weekly', 'Monthly', 'Yearly'], help="length of time covered by the report")
+    parser_cmd.add_argument('date', help="specific time covered by the report (weekly reports use YYYYMMDD, where the day used is the Sunday that week ends; monthly reports use YYYYMM; yearly reports use YYYY)")
+    parser_cmd.set_defaults(func=itc_get_pre_order_report)
 
-    parser_13 = subparsers.add_parser('generateToken', help="generate a token for accessing iTunes Connect (expires after 180 days) and optionally store it in the macOS Keychain", parents=[parser_auth_password])
-    parser_13.add_argument('--update-keychain-item', metavar="KEYCHAIN_ITEM", help='name of the macOS Keychain item in which the new access token should be stored in')
-    parser_13.set_defaults(func=itc_generate_token)
+    parser_cmd = subparsers.add_parser('generateToken', help="generate a token for accessing iTunes Connect (expires after 180 days) and optionally store it in the macOS Keychain", parents=[parser_auth_password])
+    parser_cmd.add_argument('--update-keychain-item', metavar="KEYCHAIN_ITEM", help='name of the macOS Keychain item in which the new access token should be stored in')
+    parser_cmd.set_defaults(func=itc_generate_token)
 
-    parser_14 = subparsers.add_parser('viewToken', help="display current iTunes Connect access token and its expiration date", parents=[parser_auth_password])
-    parser_14.set_defaults(func=itc_view_token)
+    parser_cmd = subparsers.add_parser('viewToken', help="display current iTunes Connect access token and its expiration date", parents=[parser_auth_password])
+    parser_cmd.set_defaults(func=itc_view_token)
 
-    parser_15 = subparsers.add_parser('deleteToken', help="delete an existing iTunes Connect access token", parents=[parser_auth_password])
-    parser_15.set_defaults(func=itc_delete_token)
+    parser_cmd = subparsers.add_parser('deleteToken', help="delete an existing iTunes Connect access token", parents=[parser_auth_password])
+    parser_cmd.set_defaults(func=itc_delete_token)
 
-    args = parser.parse_args()
+    args = parser_main.parse_args()
 
     try:
         validate_arguments(args)
     except ValueError, e:
-        parser.error(e)
+        parser_main.error(e)
 
     return args
 
